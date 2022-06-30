b45fe313990a94ba8b4e676b76de08f01e49c35e
Akash <20bcs020@iiitdmj.ac.in>
2022-06-30 20:29:36 +0530

    finailising

--- server/server/settings.py
+++ server/server/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v)!b=fa421*)wykfwqfg!gcxontn$&*lshr=f121rre3zl81x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['techakash-lms.herokuapp.com','127.0.0.1']
 
